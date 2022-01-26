fed083d1f17e56fb3de3b390f32dc3f78904535e
Sebastian Bolanos <sebasheston@protonmail.com>
2022-01-26 11:59:45 +0000

    Add nimlibbactrace and rln derivation.

--- default.nix
+++ default.nix
@@ -1,6 +1,35 @@
 { pkgs ? import (import ./nix/sources.nix).nixpkgs {} }:
 let
-  # rlnDerivation  = ./import/librln.nix
+  nimlibbacktrace = pkgs.stdenv.mkDerivation rec {
+    version = "0.0.8";
+    pname = "nim-libbacktrace";
+    src = pkgs.fetchFromGitHub {
+      fetchSubmodules = true;
+      owner = "status-im";
+      repo = "nim-libbacktrace";
+      rev = "v${version}";
+      sha256 = "CnmP46QyPsC8c/lChxpRzzITk1Ebi+V+B3mlD0W+G/c=";
+    };
+    buildPhase = ''
+      make BUILD_CXX_LIB=0
+    '';
+    installPhase = ''
+      mkdir -p $out
+      cp -r install/usr/lib $out/lib
+    '';
+  };
+  rln  = pkgs.rustPlatform.buildRustPackage rec {
+    pname = "rln";
+    version = "0.1.0";
+    src = pkgs.fetchFromGitHub {
+      owner = "kilic";
+      repo = "rln";
+      rev = "master";
+      sha256 = "q68uaBlsXPjHP65KqPen57yxsEoQNGWf8FmRlVTqWPU=";
+    };
+    cargoPatches = [ ./cargo-lock.patch ];
+    cargoSha256 = "iA9DkHZvvV1O4BsZ2HOL8ocRjEqRrrzfzbQTmrJ4msI=";
+  };
   wakunode = pkgs.stdenv.mkDerivation rec {
     version = "0.7";
     pname = "nim-waku";
@@ -11,12 +40,12 @@ let
       rev = "v${version}";
       sha256 = "nbDlPBDtASwstAKVicfFSVFmo18RguMHNApKRjdT1dU="; # pkgs.lib.fakeSha256;
     };
-    nativeBuildInputs = with pkgs; [ pcre nim libnatpmp miniupnpc ];
+    nativeBuildInputs = with pkgs; [ nim libnatpmp miniupnpc ];
+    buildInputs = with pkgs; [ pcre ];
     LIBCLANG_PATH = "${pkgs.llvmPackages.libclang}/lib";
     LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath nativeBuildInputs}";
     buildPhase = ''
       export HOME=$TMPDIR
-      make libbacktrace
       export NIMBLE_DIR=`readlink -f vendor/.nimble`
       submodules=$(cat .gitmodules | grep submodule | cut -f2 -d" " | tr -d ']"')
 
@@ -41,6 +70,8 @@ let
       # them to be
       ln -s ${pkgs.libnatpmp}/lib/libnatpmp.a vendor/nim-nat-traversal/vendor/libnatpmp-upstream/libnatpmp.a
       ln -s ${pkgs.miniupnpc}/lib/libminiupnpc.a vendor/nim-nat-traversal/vendor/miniupnp/miniupnpc/libminiupnpc.a
+      mkdir -p vendor/nim-libbacktrace/install/usr
+      ln -s ${nimlibbacktrace}/lib vendor/nim-libbacktrace/install/usr
 
       ${pkgs.nim}/bin/nim c --out:build/wakunode2 --debugger:native -d:chronicles_log_level=DEBUG --verbosity:0 --hints:off -d:release waku/v2/node/wakunode2.nim
     '';
